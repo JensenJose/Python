d7f38fcdd7f21193779ed151f44755cb3a9a26c6
Jensen <114087033+JensenJose@users.noreply.github.com>
2023-01-30 19:33:36 +0530

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,81 +1,56 @@
 import random
 
-
-exit = False
 user_points = 0
 computer_points  = 0
-name  = input("Enter your name: ")
-while(exit == False):
+name = input("Enter your name: ")
+
+while True:
     options = ["rock", "paper", "scissors"]
-    user_input  = input("Choose Rock/paper/scissors or exit: ")
+    user_input = input("Choose Rock/Paper/Scissors or Exit: ").lower()
+    if user_input not in options and user_input != "exit":
+        print("Invalid input! Please try again!!")
+        continue
     computer_input = random.choice(options)
+
+    if user_input == "exit":
+        print("Game Over")
+        break
     
-    if(user_input == "exit"):
-        print("Game Over!")
-        exit = True
+    print("Your input: ", user_input)
+    print("Computer's choice: ", computer_input)
     
-    if(user_input == "rock"):
-        if(computer_input == "rock"):
-            print("Your input is rock")
-            print("Computer input is rock")
+    if user_input == "rock":
+        if computer_input == "rock":
             print("It is a tie!")
-        elif (computer_input == "paper"):
-            print("Your input is rock")
-            print("Computer input is paper")
-            print("Computer wins!")
+        elif computer_input == "paper":
+            print("Computer Wons!!")
             computer_points += 1
-        elif(computer_input=="scissors"):
-            print("Your input is rock")
-            print("Computer input is scissors")
-            print("You win!")
+        elif computer_input == "scissors":
+            print("User Won!")
             user_points += 1
     
-    elif(user_input == "paper"):
-        if(computer_input == "rock"):
-            print("Your input is paper")
-            print("Computer input is rock")
-            print("You win!")
+    elif user_input == "paper":
+        if computer_input == "rock":
+            print("User Won!")
             user_points += 1
-        elif (computer_input == "paper"):
-            print("Your input is paper")
-            print("Computer input is paper")
+        elif computer_input == "paper":
             print("It is a tie!")
-        elif(computer_input=="scissors"):
-            print("Your input is paper")
-            print("Computer input is scissors")
-            print("Computer wins!")
+        elif computer_input == "scissors":
+            print("Computer Won!")
             computer_points += 1
             
-    elif(user_input == "scissors"):
-        if(computer_input == "rock"):
-            print("Your input is scissorsr")
-            print("Computer input is rock")
-            print("Computer wins!")
+    elif user_input == "scissors":
+        if computer_input == "rock":
+            print("Computer Won!!")
             computer_points += 1
-            computer_points += 1
-        elif (computer_input == "paper"):
-            print("Your input is scissors")
-            print("Computer input is paper")
-            print("You win!")
+        elif computer_input == "paper":
+            print("User Won!")
             user_points += 1
-        elif(computer_input=="scissors"):
-            print("Your input is scissors")
-            print("Computer input is scissors")
+        elif computer_input == "scissors":
             print("It is a tie!")
-      
-    else:
-        print("Invalid input!")
- 
-print("\n")
-print("*********************")
-print("Your points = ", user_points)
-print("Computer points = ", computer_points)   
-print("*********************")   
+            
+    print("\n***************")
+    print(name, " points: ", user_points)
+    print("Computer points: ", computer_points)
+    print("***************\n")
 
-if(user_points>computer_points):
-    print(name, " has won the game!")
-elif(user_points == computer_points):
-    print("It is a draw!")
-else:
-    print("Computer has won the game!")                 
-    
