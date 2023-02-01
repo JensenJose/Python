beaed632a772b1c3f593346aac509790e7a3be67
Jensen <114087033+JensenJose@users.noreply.github.com>
2023-02-01 19:06:20 +0530

    Update DiceRoll.py

--- DiceRoll.py
+++ DiceRoll.py
@@ -49,7 +49,7 @@ def roll_dice():
     }
      
     roll = input("Roll the dice? (Yes/No) : ")
-    while roll.lower() == "Yes". lower():
+    while roll.lower() == "yes":
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
 
